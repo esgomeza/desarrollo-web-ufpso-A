ce0cd62e1d4b90d0cc9531fb5b18e7c60a746646
esgomeza <esgomeza@ufpso.edu.co>
2026-02-21 13:33:46 -0500

    Se crea la estructura básica del proyecto

---
+++ proyecto1/script.js
@@ -0,0 +1,14 @@
+const btn= document.getElementById("themeBtn");
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+
+    if (document.body.classList.contains("dark")){
+        btn.textContent= "Modo Claro"
+   }else{
+    btn.textContent ="Modo oscuro"
+   }
+
+}
+
+)
